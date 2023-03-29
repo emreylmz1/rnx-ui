1475fbba3188c4a6d2fc4a82c0a523af38505a12
emreylmz1 <emreylmz0053@gmail.com>
2023-03-29 15:40:12 +0300

    fix.

---
+++ dist/typescript/components/Button/index.d.ts
@@ -0,0 +1,4 @@
+import { CButtonProps } from './types';
+declare function Button({ label, ...props }: CButtonProps): JSX.Element;
+export default Button;
+//# sourceMappingURL=index.d.ts.map
---
+++ dist/typescript/types/index.d.ts
@@ -0,0 +1,19 @@
+export type GenerelTypes = {
+    bg?: string;
+    p?: string | number;
+    px?: string | number;
+    py?: string | number;
+    pl?: string | number;
+    pt?: string | number;
+    pb?: string | number;
+    pr?: string | number;
+    m?: string | number;
+    mx?: string | number;
+    my?: string | number;
+    ml?: string | number;
+    mt?: string | number;
+    mb?: string | number;
+    mr?: string | number;
+    fw?: 'bold' | 'normal' | '400' | '100' | '200' | '300' | '500' | '600' | '700' | '800' | '900' | undefined;
+};
+//# sourceMappingURL=index.d.ts.map
