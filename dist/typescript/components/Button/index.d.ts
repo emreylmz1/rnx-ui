8febb859e25c8020af517f049b8cbce15a78ba4a
emreylmz1 <emreylmz0053@gmail.com>
2023-03-29 16:44:59 +0300

    fx.

--- dist/typescript/components/Button/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { CButtonProps } from './types';
-declare function Button({ label, ...props }: CButtonProps): JSX.Element;
-export default Button;
-//# sourceMappingURL=index.d.ts.map
--- dist/typescript/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { default as Button } from './components/Button';
-//# sourceMappingURL=index.d.ts.map
