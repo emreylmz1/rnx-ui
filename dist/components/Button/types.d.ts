bba8aedaa27d6f3b83135cff96b6cfc3a7db98fe
emreylmz1 <emreylmz0053@gmail.com>
2023-03-29 16:46:34 +0300

    fix.

---
+++ dist/components/Button/types.d.ts
@@ -0,0 +1,11 @@
+import { CSSProperties } from 'react';
+import { ButtonProps, ViewProps } from 'react-native';
+import { GenerelTypes } from '../../types';
+export type CButtonProps = Omit<ButtonProps, 'title'> & {
+    label?: string;
+    radius?: number;
+    styles?: {
+        root: CSSProperties;
+    };
+} & ViewProps & GenerelTypes;
+//# sourceMappingURL=types.d.ts.map
